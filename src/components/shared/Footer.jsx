e41655cbe8990b3718f896aa22819f75c40b841c
Sephine Kang <sephine.k31@gmail.com>
2025-04-05 16:12:28 -0700

    fixed footer and divider

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,11 +1,11 @@
 export default function Footer() {
   return (
     <footer className="p-6 pb-10 border-t-[1px] border-t-gray-50 flex flex-col items-center gap-4 justify-center bg-lightgrey text-mediumgrey">
-      <div className="flex justify-between items-center w-full">
+      <div className="flex justify-between items-center max-w-7xl w-[97%]">
         <span className="self-start font-medium">
           Thank you for checking in!
         </span>
-        <div className="flex flex-col text-xs gap-1">
+        <div className="flex flex-col text-sm gap-1">
           <span>sephine.k31@gmail.com</span>
           <span>+1 213.378.5068</span>
         </div>
